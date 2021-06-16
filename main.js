9c4bf11e8affc2af75291a648a6e9778dab03800
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2021-06-16 13:46:36 +0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', async () => {
+	await Swal.fire({
+		title: 'Hallo, Dian!',
+		text: 'Klik OK YA!',
+		imageUrl: './img/foto.jpg',
+		imageWidth: 250,
+		imageHeight: 300,
+		imageAlt: 'Custom image',
+	});
+	await Swal.fire('Semangat Kuliah!');
+	await Swal.fire('Nonton Drakor Dikurangin!');
+	const promise = await Swal.fire({
+		title: 'Janji gak ?',
+		showDenyButton: true,
+		confirmButtonText: `IYA PANN`,
+		denyButtonText: `GAMAU`,
+	});
+	if (promise.isConfirmed) {
+		await Swal.fire({
+			icon: 'error',
+			title: 'ahh...',
+			text: 'Janji doang jemput cowok depan gang!',
+			footer: 'HAHAHAHAA',
+			confirmButtonText: `ZZZZ`,
+		});
+		await Swal.fire({
+			title: 'Tapi serius',
+			text: 'Kebanyakan depan laptop ntar min',
+		});
+		await Swal.fire('Kurang perhatian apalagi saya HAHAHA');
+		await Swal.fire('Udah ah');
+		await Swal.fire('Mo Lanjut belajar');
+		await Swal.fire({
+			title: 'BYE',
+			imageUrl: './img/foto-2.jpg',
+			imageWidth: 250,
+			imageHeight: 300,
+			imageAlt: 'Custom image',
+		});
+	}
+
+	if (promise.isDenied) {
+		await Swal.fire({
+			icon: 'error',
+			title: 'YAUDA SAMPE SINI...',
+			text: 'Byeee!',
+			confirmButtonText: `:0`,
+		});
+	}
+});

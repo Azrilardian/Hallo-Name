ffa60ad2693363e13264fd91c43c8574a44cdf82
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2021-09-06 17:52:16 +0800

    feat: update iraaa

--- main.js
+++ main.js
@@ -2,51 +2,58 @@ const picture = document.querySelector('.picture');
 
 document.addEventListener('DOMContentLoaded', async () => {
 	await Swal.fire({
-		title: 'Hallo, Dian!',
-		text: 'Klik OK YA!',
-		imageUrl: './img/foto.jpg',
-		imageWidth: 250,
-		imageHeight: 300,
-		imageAlt: 'Custom image',
+		title: 'Iraaaaaaa',
+		text: 'Klik ok',
 	});
-	await Swal.fire('Semangat Kuliah!');
-	await Swal.fire('Nonton Drakor Dikurangin!');
+	await Swal.fire('Ira jelek');
+	await Swal.fire('Suka tbtb serius');
+	await Swal.fire('Suka ga nyapa');
+	await Swal.fire('Tapi saya sayang xixixixixixi');
+	await Swal.fire(':0');
 	const promise = await Swal.fire({
-		title: 'Janji gak ?',
+		title: 'Becanda ga ra ?',
 		showDenyButton: true,
-		confirmButtonText: `IYA PANN`,
-		denyButtonText: `GAMAU`,
+		confirmButtonText: `MAUNYA SERIUSS`,
+		denyButtonText: `BECANDAIN`,
 	});
 	if (promise.isConfirmed) {
 		await Swal.fire({
 			icon: 'error',
 			title: 'ahh...',
-			text: 'Janji doang jemput cowok depan gang!',
-			footer: 'HAHAHAHAA',
-			confirmButtonText: `ZZZZ`,
+			text: 'Serius tapi suka ninggal tidur',
+			confirmButtonText: `xixixi`,
 		});
+		await Swal.fire('Ngorok juga si');
+		await Swal.fire('BECANDA.');
 		await Swal.fire({
-			title: 'Tapi serius',
-			text: 'Kebanyakan depan laptop ntar min',
+			title: 'Jan boros boros, rajin rajin kuliahh',
+			text: 'Jan main hp terus juga',
 		});
-		await Swal.fire('Kurang perhatian apalagi saya HAHAHA');
+		await Swal.fire('Hemmm');
 		await Swal.fire('Udah ah');
-		await Swal.fire('Mo Lanjut belajar');
+		await Swal.fire('Mo Lanjut kerja');
+		await Swal.fire('Lagi gabut aja ini');
+		await Swal.fire('Kerjaan juga uda slse si');
+		await Swal.fire('Byeeee');
+		await Swal.fire('Ee tapi kenalin dulu');
 		await Swal.fire({
-			title: 'BYE',
-			imageUrl: './img/foto-2.jpg',
+			title: 'Aamiin paling serius saya',
+			imageUrl: './img/ira.png',
 			imageWidth: 250,
 			imageHeight: 300,
 			imageAlt: 'Custom image',
 		});
+		await Swal.fire('xixixi');
+		await Swal.fire('bye byee');
+
 		picture.setAttribute('src', './img/crop.jpg');
 	}
 
 	if (promise.isDenied) {
 		await Swal.fire({
 			icon: 'error',
-			title: 'YAUDA SAMPE SINI...',
-			text: 'Byeee!',
+			title: 'males dah :(',
+			text: 'gaasik amu',
 			confirmButtonText: `:0`,
 		});
 	}
